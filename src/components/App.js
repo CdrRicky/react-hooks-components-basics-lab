299b29dd7a23a28ed8da2cc22613bb16c5ca84fa
Dev_Ricky <eric.kiogora12@gmail.com>
2022-08-01 08:30:39 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,6 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
 function About() {
   return (
     <div id="about">
@@ -25,12 +24,11 @@ function About() {
   );
 }
 
+
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
       <Home />
       <About />
     </div>
